46dea7a98ecb03d0d02f32d0d78995bc67def2c2
Caio <caio.abreu@sou.inteli.edu.br>
2024-02-06 00:08:25 -0300

    Revert "fix(docusaurus-deploy): :fire: Removes blog/ folder from docusaurus template"
    This reverts commit 7dec5f9bb6b7eab8afc5a1fc55146ac6d582021c.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -46,6 +46,13 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -71,6 +78,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
+          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -110,6 +118,10 @@ const config = {
           {
             title: 'More',
             items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
