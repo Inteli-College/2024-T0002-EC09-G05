c441a250f9df6a89ab17dc6cfb77889fbb17b5fc
Caio <caio.abreu@sou.inteli.edu.br>
2024-02-06 00:18:16 -0300

    fix(docusaurus-deploy): :bug: Fixes source path to docusaurus

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -40,7 +40,8 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
+          routeBasePath: '/',
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
