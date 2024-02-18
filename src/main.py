3fbdb0c966bfdfc45be46764d459dbd4d4e3e2ae
joão Vitor oliveira Rodrigues <joao.rodrigues@sou.inteli.edu.br>
2024-02-18 14:37:32 -0300

    add Sensor code

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from MQTT_Publisher import Sensor
+
+pub_broker = {
+    "link":"broker.hivemq.com",
+    "port":1883
+}
+
+temperature_sensor = Sensor(
+    name="temperatura",
+    min = -40,
+    max=70
+)
+
+
+
+if __name__ == "__main__":
+    temperature_sensor.on(pub_broker)
+
