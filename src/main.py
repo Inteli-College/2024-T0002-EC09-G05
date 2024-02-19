18d50384073c471880060732f8ade8a0f5bae373
MihaellK <79888949+MihaellK@users.noreply.github.com>
2024-02-18 21:15:31 -0300

    feat (tests): add tests v1
    documentado no README na pasta tests

--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
-from MQTT_Publisher import Sensor
+from mqtt.MQTT_Publisher import Sensor
 
 pub_broker = {
-    "link": "broker.hivemq.com",
-    "port": 1883
+    "link":"broker.hivemq.com",
+    "port":1883
 }
 
 temperature_sensor = Sensor(
     name="temperatura",
-    min=-40,
-    max=70,
-    broker=pub_broker
+    min = -40,
+    max=70
 )
 
+
+
 if __name__ == "__main__":
-    temperature_sensor.on()
+    temperature_sensor.on(pub_broker)
+
