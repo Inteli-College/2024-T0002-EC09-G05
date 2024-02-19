f897f5ca530fe189f718edb0bae644ab984a97ee
Paulo <paulo.evangelista@sou.inteli.edu.br>
2024-02-18 21:26:58 -0300

    feat(src): ✨ improved simulator and code organization

--- src/server/main.go
+++ src/server/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
-	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
+	"os"
+	"time"
+
 	influx "github.com/influxdata/influxdb-client-go/v2"
 	"github.com/influxdata/influxdb-client-go/v2/api"
-	"context"
-	"time"
-	"encoding/json"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type SensorData struct {
@@ -18,44 +20,68 @@ type SensorData struct {
 	PM2_5     float32 `json:"pm2_5"`
 	PM4_0     float32 `json:"pm4_0"`
 	PM10_0    float32 `json:"pm10_0"`
-	Temp      int     `json:"temp"`
-	Hum       int     `json:"hum"`
+	Temp      float32 `json:"temp"`
+	Hum       float32 `json:"hum"`
 }
-
 
 func saveDataToInfluxDB(data SensorData, writeAPI api.WriteAPIBlocking) {
 	p := influx.NewPointWithMeasurement("medicoes").
-			AddTag("sensor_id", data.Sensor_id).
-			AddField("hum", data.Hum).
-			AddField("temp", data.Temp).
-			AddField("pm2_5", data.PM2_5).
-			AddField("pm1_0", data.PM1_0).
-			AddField("pm4_0", data.PM4_0).
-			AddField("pm10_0", data.PM10_0).
-			SetTime(time.Unix(data.Timestamp, 0))
-	
-		// Escreve o ponto
-		writeAPI.WritePoint(context.Background(), p)
+		AddTag("sensor_id", data.Sensor_id).
+		AddField("hum", data.Hum).
+		AddField("temp", data.Temp).
+		AddField("pm2_5", data.PM2_5).
+		AddField("pm1_0", data.PM1_0).
+		AddField("pm4_0", data.PM4_0).
+		AddField("pm10_0", data.PM10_0).
+		SetTime(time.Unix(data.Timestamp, 0))
+
+	// Escreve o ponto
+	err := writeAPI.WritePoint(context.Background(), p)
+	if err != nil {
+		fmt.Println("ERRO NA OPERACAO DE WRITE")
+		fmt.Println(err.Error())
+	}
 }
 
-func main() {
+func setupInflux() api.WriteAPIBlocking {
 
-	client := influx.NewClient("http://influxdb:8086", "admin")
 	org := "iot"
 	bucket := "sensor"
-	writeAPI := client.WriteAPIBlocking(org, bucket)
 
+	client := influx.NewClientWithOptions("http://influxdb:8086", "admin",
+		influx.DefaultOptions().SetBatchSize(1000).SetUseGZip(true).SetPrecision(time.Second))
+
+	organization, _ := client.OrganizationsAPI().FindOrganizationByName(context.Background(), "iot")
+
+	_, err := client.BucketsAPI().FindBucketByName(context.Background(), "sensor")
+	if err != nil && err.Error() != "bucket 'sensor' not found"{
+
+		fmt.Println("Erro ao buscar o bucket, tentando criar um novo")
+		_, err := client.BucketsAPI().CreateBucketWithName(context.Background(), organization, bucket)
+		if err != nil {
+			fmt.Println("Erro ao criar o bucket")
+			fmt.Println(err.Error())
+		}
+
+	} else {
+		fmt.Println("Bucket encontrado, continuando...")
+	}
+
+
+	return client.WriteAPIBlocking(org, bucket)
+}
+
+func connectRabbitMQ() <-chan amqp.Delivery {
 	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
 	if err != nil {
 		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+		defer conn.Close()
 	}
-	defer conn.Close()
-
 	ch, err := conn.Channel()
 	if err != nil {
 		log.Fatalf("Failed to open a channel: %v", err)
+		defer ch.Close()
 	}
-	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
 		"MQTTQueue", // name of the queue
@@ -68,8 +94,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to declare a queue: %v", err)
 	}
-	
-	msgs, err := ch.Consume(
+
+	messages, _ := ch.Consume(
 		q.Name, // queue
 		"",     // consumer
 		true,   // auto-acknowledge
@@ -82,20 +108,48 @@ func main() {
 		log.Fatalf("Failed to register a consumer: %v", err)
 	}
 
-	forever := make(chan bool)
+	return messages
+}
 
-	go func() {
-		for d := range msgs {
-			fmt.Printf("Received a message: %s\n", d.Body)
-			var data SensorData
-			err := json.Unmarshal(d.Body, &data)
-			if err != nil {
-				fmt.Println("Error parsing JSON: ", err)
-			}
-			saveDataToInfluxDB(data, writeAPI)
+func handleSensorData( rabbitMessages <-chan amqp.Delivery, influxWriteAPI api.WriteAPIBlocking) {
+	batchCounter := 0
+
+	contagemIDs := make(map[string]int)
+
+	fmt.Println("Waiting for messages...")
+
+	for d := range rabbitMessages {
+		
+		os.Stdout.Sync()
+		var data SensorData
+		err := json.Unmarshal(d.Body, &data)
+
+		contagemIDs[data.Sensor_id]++
+
+		if err != nil {
+			fmt.Println("Error parsing JSON: ", err)
 		}
-	}()
+		saveDataToInfluxDB(data, influxWriteAPI)
+		
+		batchCounter++
+		if batchCounter == 1000 {
+			fmt.Println("--> Flushed batch (1000 points) to InfluxDB")
+			fmt.Println("	", contagemIDs)
+			batchCounter = 0
+		} else if batchCounter%100 == 0 {
+			fmt.Printf("Filling batch... (%d/1000)\n", batchCounter)
+		}
+	}
+}
 
-	fmt.Println(" [*] Waiting for messages")
-	<-forever
+func main() {
+
+	influxWriteAPI := setupInflux()
+
+	rabbitMessages := connectRabbitMQ()
+
+	go handleSensorData(rabbitMessages, influxWriteAPI)
+
+	select {}
+	
 }
--- src/simulator/main.go
+++ src/simulator/main.go
@@ -9,60 +9,97 @@ import (
 )
 
 type SensorData struct {
-	Sensor_id string  `json:"sensor_id"`
-	Timestamp int     `json:"timestamp"`
+	SensorId  string  `json:"sensor_id"`
+	Timestamp int64   `json:"timestamp"`
 	PM1_0     float32 `json:"pm1_0"`
 	PM2_5     float32 `json:"pm2_5"`
 	PM4_0     float32 `json:"pm4_0"`
 	PM10_0    float32 `json:"pm10_0"`
-	Temp      int     `json:"temp"`
-	Hum       int     `json:"hum"`
+	Temp      float32 `json:"temp"`
+	Hum       float32 `json:"hum"`
 }
 
-func simulateSensorData() SensorData {
+func generateSensorData(lastData SensorData) SensorData {
 	return SensorData{
-		Sensor_id: "sensor1",
-		Timestamp: int(time.Now().Unix()),
-		PM1_0:     rand.Float32() * 50,
-		PM2_5:     rand.Float32() * 50,
-		PM4_0:     rand.Float32() * 50,
-		PM10_0:    rand.Float32() * 50,
-		Temp:      rand.Intn(120) - 20,
-		Hum:       rand.Intn(120) - 20,
+		SensorId:  lastData.SensorId,
+		Timestamp: time.Now().Unix(),
+		PM1_0:     lastData.PM1_0 + rand.Float32()*1.5 - 0.75,
+		PM2_5:     lastData.PM2_5 + rand.Float32() - 0.5,
+		PM4_0:     lastData.PM4_0 + rand.Float32() - 0.5,
+		PM10_0:    lastData.PM10_0 + rand.Float32() - 0.5,
+		Temp:      lastData.Temp + rand.Float32() - 0.5,
+		Hum:       lastData.Hum + rand.Float32() - 0.5,
 	}
 }
 
-var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
-	fmt.Println("Connected")
-}
-
-var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
-	fmt.Printf("Connect lost: %v", err)
-}
-
-func main() {
+func configureClient(clientName string) mqtt.Client {
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker("rabbitmq:1883")
-	opts.SetClientID("go_mqtt_client")
+	opts.SetClientID(clientName)
 	opts.SetUsername("guest")
 	opts.SetPassword("guest")
-	opts.OnConnect = connectHandler
-	opts.OnConnectionLost = connectLostHandler
+
+	opts.OnConnect = func(client mqtt.Client) {
+		fmt.Println("Connected")
+	}
+
+	opts.OnConnectionLost = func(client mqtt.Client, err error) {
+		fmt.Printf("Connect lost: %v", err)
+	}
+
 	client := mqtt.NewClient(opts)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
 
+	return client
+}
+
+func simulateSensor(clientName string) {
+
+	client := configureClient(clientName)
+
+	currentData := SensorData{
+		SensorId:  clientName,
+		Timestamp: time.Now().Unix(),
+		PM1_0:     100,
+		PM2_5:     75,
+		PM4_0:     75,
+		PM10_0:    50,
+		Temp:      20,
+		Hum:       40,
+	}
+
+	lastData := currentData
+
 	for {
 
-		topic := "sensor/data"
-		data, _ := json.Marshal(simulateSensorData())
+		time.Sleep(time.Duration(1 * time.Second))
 
-		if token := client.Publish(topic, 1, true, data); token.Wait() && token.Error() != nil {
+		currentData = generateSensorData(lastData)
+
+		lastData = currentData
+
+		jsonData, _ := json.Marshal(currentData)
+
+		if token := client.Publish("sensor/data", 1, true, jsonData); token.Wait() && token.Error() != nil {
 			panic(token.Error())
 		}
 
-		fmt.Println("Sent simulated sensor reading to topic", topic)
-		time.Sleep(3 * time.Second)
 	}
+}
+
+func main() {
+	go simulateSensor("Liberdade")
+	go simulateSensor("Luz")
+	go simulateSensor("Butantã")
+	go simulateSensor("Morumbi")
+	go simulateSensor("Pinheiros")
+	go simulateSensor("Ipiranga")
+	go simulateSensor("Parelheiros")
+	go simulateSensor("Grajaú")
+	go simulateSensor("Moema")
+	go simulateSensor("Vila Mariana")
+	go simulateSensor("Paulista")
+	select {}
 }
