0c53e49aa79adbbcfd5b4b16deb355d8325e75dc
JvLetsBora <99259911+JvLetsBora@users.noreply.github.com>
2024-03-13 08:17:38 -0300

    ...

--- src/frontend/grupo5/src/main.js
+++ src/frontend/grupo5/src/main.js
@@ -1,7 +1,9 @@
-import './assets/main.css'
+import './index.css'
+
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+    unstyled: true
+})
 
 app.mount('#app')
---
+++ src/frontend/grupo5/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  
+  content: [
+        // Example content paths...
+        './public/**/*.html',
+        './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  
+}
+
