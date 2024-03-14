9e592ba61e2771dc52984e759095a97af3e95e79
ViniciosLugli <vinicioslugli@gmail.com>
2024-03-14 10:04:51 -0300

    refactor(frontend): :lipstick: Updated logo

--- src/frontend/src/views/LoginView.vue
+++ src/frontend/src/views/LoginView.vue
@@ -6,11 +6,7 @@
       <div class="bg-white max-w-xl min-w-96 rounded-2xl">
         <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              class="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
+            <img class="mx-auto" src="../assets/logo.png" alt="Logo" height="128" width="128" />
             <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
               Sign in to your account
             </h2>
