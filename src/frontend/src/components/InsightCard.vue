f3d838cbc8f5532f8b2fce77d24c0d2eb9e95c3e
MihaellK <79888949+MihaellK@users.noreply.github.com>
2024-03-14 10:08:29 -0300

    feat(frontend): add component InsightCard

---
+++ src/frontend/src/components/InsightCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <CCard class="mb-3" style="max=width: 540px">
+    <CRow class="g-0">
+        <CCol :md="4">
+        <CCardImage class="rounded-0" src="/images/vue.jpg" />
+        </CCol>
+        <CCol :md="8">
+        <CCardBody>
+            <CCardTitle>Card title</CCardTitle>
+            <CCardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CCardText>
+            <CCardText><small class="text-body-secondary">Last updated 3 mins ago</small></CCardText>
+        </CCardBody>
+        </CCol>
+    </CRow>
+    </CCard>
+</template>
