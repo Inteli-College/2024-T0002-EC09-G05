1fcb661ce8e4c457c6c7824dac3cac2f33879ad3
JvLetsBora <99259911+JvLetsBora@users.noreply.github.com>
2024-03-18 10:34:31 -0300

    CREATE - Adição da lógica de autenticação no frontend

--- src/frontend/src/components/DashboardContainer.vue
+++ src/frontend/src/components/DashboardContainer.vue
@@ -9,7 +9,7 @@ import MainChart from './MainChart.vue';
         <div class=" bg-slate-100 sideMenu"></div>
         <SideBar />
 
-        <div class="bg-slate-100 xl:col-start-2 xl:col-end-9 flex-col min-h-screen col-start-1 h-fit  col-end-9 w-full">
+        <div class="bg-white xl:col-start-2 xl:col-end-9 flex-col min-h-screen col-start-1 h-fit  col-end-9 w-full">
             
             <div class="flex flex-col gap-10 h-full max-w-full m-10 items-center align-items: flex-start">
                 <div style="width: 100%; max-height: 300px; height: 300px;">
--- src/frontend/src/views/DefaultView.vue
+++ src/frontend/src/views/DefaultView.vue
@@ -1,14 +1,20 @@
 <script setup>
-import HeaderBar from '../components/HeaderBar.vue'
-import DashboardContainer from '../components/DashboardContainer.vue'
+
+import HeaderBar from '../components/HeaderBar.vue';
+import DashboardContainer from '../components/DashboardContainer.vue';
+
+//http://10.254.17.50:3000/app
+
+function apiData(){
+  return false
+}
 
 </script>
-<template>
-  <div class="default" >
-    <HeaderBar />
-    <DashboardContainer />
 
+<template>
+  <div class="default">
+    <!-- Renderiza os componentes somente se apiData for definido -->
+    <HeaderBar v-if="apiData" />
+    <DashboardContainer v-if="apiData" />
   </div>
 </template>
-
-
