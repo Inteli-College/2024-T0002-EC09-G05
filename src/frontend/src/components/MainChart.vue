c7cda1133b806fa3d4c00cfa629341cc9412c67d
cmtabr <caio.abreu@sou.inteli.edu.br>
2024-03-28 09:01:44 -0300

    fix(chart-exception): :bug: fixes data fetching from endpoint and returned values

--- src/frontend/src/components/MainChart.vue
+++ src/frontend/src/components/MainChart.vue
@@ -6,65 +6,111 @@
 
 <script setup>
 import { Chart, registerables } from 'chart.js';
-import { onMounted, ref } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
+import axios from 'axios';
 
 Chart.register(...registerables);
 
 const chartRef = ref(null);
+let isFetching = false;
+let fetchDataInterval = null;
 
 async function fetchData() {
+  if (isFetching) {
+    return;
+  }
+
+  isFetching = true;
   const url = '/api/data/getDataByRelativeTime';
   const requestBody = {
-    start_time: 60, // Puxa os dados da última hora
-    aggregator: 30, // Média dos dados a cada 30 segundos
-    fields: ['temp'], // Dados de temperatura e umidade
-    sensors: ['Pinheiros'],
+    start_time: 5,
+    aggregator: 30,  
+    fields: ['temp'],  
+    sensors: ['Pinheiros'],  
   };
+  let labels;
+  let values;
 
   try {
-    const response = await fetch(url, {
-      method: 'POST',
+    const response = await axios.post(url, requestBody, {
       headers: {
         'Content-Type': 'application/json',
-        //'Token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE3MTI0MDQ1NDAsInJvbGUiOjEsInVzZXJfaWQiOjF9.jndAxkhb-6OZloqVyi6ZyA3pnHRjFJ'
+        'Token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE3MTI0NDc5NTUsInJvbGUiOjEsInVzZXJfaWQiOjF9.Uhlb9g6aMonA78FUZyw06Y4VftpKglY-VxiXb_ycxTw', 
       },
-      body: JSON.stringify(requestBody),
     });
 
-    if (!response.ok) throw new Error('Network response was not ok');
+    if (response.data && response.data.length > 0) {
+      const sensorData = response.data[0].data;
+      labels = sensorData.map(entry => {
+        const correctedTime = entry.time.replace(" +0000 UTC", ""); 
+        console.log("Corrected Time: ", correctedTime)
+        const date = new Date(correctedTime);
+        return date.toLocaleTimeString();
+      });
+      values = sensorData.map(entry => entry.value);
 
-    const data = await response.json();
-
-    const labels = data.map(entry => new Date(entry.time).toLocaleTimeString());
-    const values = data.map(entry => entry.value);
-
-    if (chartRef.value) {
-      chartRef.value.data.labels = labels;
-      chartRef.value.data.datasets[0].data = values;
-      chartRef.value.update();
-    }
+      console.log('Data: ', sensorData)
+      console.log('Labels:', labels)
+      console.log('Values:', values)
+      
+      if (chartRef.value) {
+        console.log("Data received")
+      }
+      } else {
+        console.warn('Received data is empty or in an unexpected format:', response.data);
+      }
+    return { labels, values };
   } catch (error) {
-    console.error('There was a problem with the fetch operation:', error);
-  }
+    console.error('Error:', error);
+  } finally {
+    isFetching = false;
+}}
+
+async function updateChart(chart){
+    const data = await fetchData()
+    console.log("Flush: ", data)
+    data.labels.forEach((label, index) => {
+        chart.data.labels.push(label);
+        chart.data.datasets.forEach((dataset) => {
+            if(data.values.length > index){
+                dataset.data.push(data.values[index]);
+            }
+        });
+    });
+    chart.update();
 }
 
 onMounted(() => {
-  const ctx = document.getElementById('chart').getContext('2d');
-  chartRef.value = new Chart(ctx, {
+  const ctx = document.getElementById('chart');
+  let chartRef = new Chart(ctx, {
     type: 'line',
     data: {
       labels: [],
       datasets: [{
-        label: 'Dados do Sensor',
+        label: 'Temperatura',
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
         borderColor: 'rgba(255, 99, 132, 1)',
         data: [],
       }],
     },
     options: {
+      scales: {
+        y: {
+          display: true,
+        },
+        x: {
+          display: true,
+        }
+      },
     },
   });
-
-  fetchData();
+  console.log("Chart Ref: ", chartRef)
+  updateChart(chartRef);
+  fetchDataInterval = setInterval(fetchData, 1000);
 });
+
+onUnmounted(() => {
+  clearInterval(fetchDataInterval);
+});
+
 </script>
